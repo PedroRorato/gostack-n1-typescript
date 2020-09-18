4c98ce038a21e6e6fc2092840abc18387db9ba72
Pedro <pedroberorato@gmail.com>
2020-09-17 22:58:09 -0300

    Tipagem

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import express from 'express';
+import createUser from './services/CreateUser';
 
 const app = express();
 
 app.get('/', (request, response) => {
+    const user = createUser({
+        email: 'pedroberorato@gmail.com',
+        password: '12345',
+        techs: [
+            'Node.js', 
+            'ReactJS',
+            { title: 'JavaScript', experience: 100 },
+        ]
+    });
+
+    console.log(user.email);
+
     return response.json({ message: 'Hello World'});
 });
+
+
 
 app.listen(3333);
